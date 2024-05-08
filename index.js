280a4ac8d7b2d762c70b5f40ce3c2b2cbe0bc1be
hoanganhhhhh12 <streborjukpun0suy@hotmail.com>
2024-05-09 05:58:59 +0700

    feat task

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function canCross(stones) {
-  const map = new Map();
-  for (const stone of stones) map.set(stone, new Set());
-  map.get(0).add(0);
-  for (let i = 0; i < stones.length; i++) {
-    for (const step of map.get(stones[i])) {
-      for (let k = step - 1; k <= step + 1; k++) {
-        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
-      }
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return map.get(stones[stones.length - 1]).size > 0;
+  return false;
 }
