7cd587619df1074b2e559fe684a7ee063951b0f9
hoanganhhhhh12 <streborjukpun0suy@hotmail.com>
2024-05-09 18:06:07 +0700

    init project

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
