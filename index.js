838da3b9e090a36164c342f06ec076f26081a9d7
hoanganhhhhh12 <streborjukpun0suy@hotmail.com>
2024-05-12 23:54:34 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-    arr.slice(i * size, i * size + size),
-  );
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
